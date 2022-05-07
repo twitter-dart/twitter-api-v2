4cfcd6b8d0a82a45a9612bfc4be367ef00bfe497
myConsciousness <kato.shinya.dev@gmail.com>
2022-05-07 20:30:00 +0900

    refactor: Organized import orders

--- lib/twitter_api_v2.dart
+++ lib/twitter_api_v2.dart
@@ -2,23 +2,21 @@
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided the conditions.
 
-export 'package:twitter_api_v2/src/twitter_api.dart';
-export 'package:twitter_api_v2/src/twitter_exception.dart';
 export 'package:twitter_api_v2/src/client/oauth_tokens.dart';
-
-// Response
-export 'package:twitter_api_v2/src/service/twitter_response.dart';
-export 'package:twitter_api_v2/src/service/tweets/tweet_data.dart';
-export 'package:twitter_api_v2/src/service/tweets/tweet_meta.dart';
-export 'package:twitter_api_v2/src/service/tweets/tweet_count_data.dart';
-export 'package:twitter_api_v2/src/service/tweets/tweet_count_meta.dart';
-export 'package:twitter_api_v2/src/service/users/user_data.dart';
-export 'package:twitter_api_v2/src/service/users/user_meta.dart';
+export 'package:twitter_api_v2/src/service/compliance/compliance_data.dart';
+export 'package:twitter_api_v2/src/service/compliance/job_status.dart';
+export 'package:twitter_api_v2/src/service/compliance/job_type.dart';
+export 'package:twitter_api_v2/src/service/lists/list_data.dart';
+export 'package:twitter_api_v2/src/service/lists/list_meta.dart';
 export 'package:twitter_api_v2/src/service/spaces/space_data.dart';
 export 'package:twitter_api_v2/src/service/spaces/space_meta.dart';
 export 'package:twitter_api_v2/src/service/spaces/space_state.dart';
-export 'package:twitter_api_v2/src/service/lists/list_data.dart';
-export 'package:twitter_api_v2/src/service/lists/list_meta.dart';
-export 'package:twitter_api_v2/src/service/compliance/compliance_data.dart';
-export 'package:twitter_api_v2/src/service/compliance/job_type.dart';
-export 'package:twitter_api_v2/src/service/compliance/job_status.dart';
+export 'package:twitter_api_v2/src/service/tweets/tweet_count_data.dart';
+export 'package:twitter_api_v2/src/service/tweets/tweet_count_meta.dart';
+export 'package:twitter_api_v2/src/service/tweets/tweet_data.dart';
+export 'package:twitter_api_v2/src/service/tweets/tweet_meta.dart';
+export 'package:twitter_api_v2/src/service/twitter_response.dart';
+export 'package:twitter_api_v2/src/service/users/user_data.dart';
+export 'package:twitter_api_v2/src/service/users/user_meta.dart';
+export 'package:twitter_api_v2/src/twitter_api.dart';
+export 'package:twitter_api_v2/src/twitter_exception.dart';
