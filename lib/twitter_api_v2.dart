08a86f5d8df6f673a1fdeb948681772d22d6c18d
myConsciousness <kato.shinya.dev@gmail.com>
2022-05-15 09:24:59 +0900

    fix: fixed for the issue (#122)

--- lib/twitter_api_v2.dart
+++ lib/twitter_api_v2.dart
@@ -4,19 +4,24 @@
 
 export 'package:twitter_api_v2/src/client/oauth_tokens.dart';
 export 'package:twitter_api_v2/src/service/compliance/compliance_data.dart';
+export 'package:twitter_api_v2/src/service/compliance/compliance_service.dart';
 export 'package:twitter_api_v2/src/service/compliance/job_status.dart';
 export 'package:twitter_api_v2/src/service/compliance/job_type.dart';
 export 'package:twitter_api_v2/src/service/lists/list_data.dart';
 export 'package:twitter_api_v2/src/service/lists/list_meta.dart';
+export 'package:twitter_api_v2/src/service/lists/lists_service.dart';
 export 'package:twitter_api_v2/src/service/spaces/space_data.dart';
 export 'package:twitter_api_v2/src/service/spaces/space_meta.dart';
 export 'package:twitter_api_v2/src/service/spaces/space_state.dart';
+export 'package:twitter_api_v2/src/service/spaces/spaces_service.dart';
 export 'package:twitter_api_v2/src/service/tweets/tweet_count_data.dart';
 export 'package:twitter_api_v2/src/service/tweets/tweet_count_meta.dart';
 export 'package:twitter_api_v2/src/service/tweets/tweet_data.dart';
 export 'package:twitter_api_v2/src/service/tweets/tweet_meta.dart';
+export 'package:twitter_api_v2/src/service/tweets/tweets_service.dart';
 export 'package:twitter_api_v2/src/service/twitter_response.dart';
 export 'package:twitter_api_v2/src/service/users/user_data.dart';
 export 'package:twitter_api_v2/src/service/users/user_meta.dart';
+export 'package:twitter_api_v2/src/service/users/users_service.dart';
 export 'package:twitter_api_v2/src/twitter_api.dart';
 export 'package:twitter_api_v2/src/twitter_exception.dart';
