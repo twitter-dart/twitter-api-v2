e60dec8b921e3f59094f1d937f51cec26433a33c
Kato Shinya <kato.shinya.dev@gmail.com>
2022-05-20 09:50:47 +0900

    feat: fix for the issue (#141)

--- lib/src/service/tweets/tweet_data.dart
+++ lib/src/service/tweets/tweet_data.dart
@@ -30,4 +30,28 @@ class TweetData with _$TweetData {
 
   factory TweetData.fromJson(Map<String, Object?> json) =>
       _$TweetDataFromJson(json);
+
+  /// Returns true if this data has referenced tweets, otherwise false.
+  bool get hasReferencedTweets => referencedTweets != null;
+
+  /// Returns true if this data has not referenced tweets, otherwise false.
+  bool get hasNotReferencedTweets => !hasReferencedTweets;
+
+  /// Returns true if this data has entities object, otherwise false.
+  bool get hasEntities => entities != null;
+
+  /// Returns true if this data has not entities object, otherwise false.
+  bool get hasNotEntities => !hasEntities;
+
+  /// Returns true if this data has attachments object, otherwise false.
+  bool get hasAttachments => attachments != null;
+
+  /// Returns true if this data has not attachments object, otherwise false.
+  bool get hasNotAttachments => !hasAttachments;
+
+  /// Returns true if this data has geo object, otherwise false.
+  bool get hasGeo => geo != null;
+
+  /// Returns true if this data has not geo object, otherwise false.
+  bool get hasNotGeo => !hasGeo;
 }
