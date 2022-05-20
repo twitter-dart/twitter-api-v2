eb5a757b515fb3cb1645aedb92ff6be48b1c2c21
Kato Shinya <kato.shinya.dev@gmail.com>
2022-05-20 10:08:48 +0900

    fix: fixed for the issue (#141)

--- lib/src/service/users/user_expansion.dart
+++ lib/src/service/users/user_expansion.dart
@@ -2,6 +2,7 @@
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided the conditions.
 
+// Project imports:
 import '../expansion.dart';
 
 /// Represents an extensible field about User.
