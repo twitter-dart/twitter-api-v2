ea5cf9806842e42e654ff7bd8f104420a5191e60
Roberto Doering <rbydoering@gmail.com>
2022-05-21 19:18:57 +0200

    test: Added send stub

--- test/mocks/client_context_stubs.dart
+++ test/mocks/client_context_stubs.dart
@@ -3,14 +3,15 @@
 // modification, are permitted provided the conditions.
 
 // Dart imports:
+import 'dart:convert';
 import 'dart:io';
 
 // Package imports:
 import 'package:http/http.dart' as http;
 import 'package:mockito/mockito.dart';
-
 // Project imports:
 import 'package:twitter_api_v2/src/client/user_context.dart';
+
 import 'mock.mocks.dart';
 
 MockClientContext buildGetStub(
@@ -107,6 +108,43 @@ MockClientContext buildPutStub(
       200,
       headers: {'content-type': 'application/json; charset=utf-8'},
     ),
+  );
+
+  //! Override if you want to test with OAuth 1.0a.
+  when(mockClientContext.hasOAuth1Client).thenReturn(false);
+
+  return mockClientContext;
+}
+
+MockClientContext buildSendStub(
+  final UserContext userContext,
+  final String resourcePath, [
+  final Map<String, String>? queryParameters,
+]) {
+  final mockClientContext = MockClientContext();
+
+  Stream<List<int>> responseStream() async* {
+    final converter = JsonUtf8Encoder();
+    final responsesString = await File(resourcePath).readAsString();
+    final responses = jsonDecode(responsesString) as List<dynamic>;
+
+    for (final response in responses) {
+      yield converter.convert(response);
+    }
+  }
+
+  when(mockClientContext.send(
+    userContext,
+    any,
+    timeout: anyNamed('timeout'),
+  )).thenAnswer(
+    (_) async {
+      return http.StreamedResponse(
+        responseStream(),
+        200,
+        headers: {'content-type': 'application/json; charset=utf-8'},
+      );
+    },
   );
 
   //! Override if you want to test with OAuth 1.0a.
