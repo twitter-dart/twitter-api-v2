14fbb3c451b1e49f8c19b3238e44273cc0f4488d
Roberto Doering <rbydoering@gmail.com>
2022-05-21 17:57:05 +0200

    feat: Updated TwitterException

--- lib/src/twitter_exception.dart
+++ lib/src/twitter_exception.dart
@@ -9,30 +9,46 @@ import 'package:http/http.dart';
 /// with the Twitter API.
 class TwitterException implements Exception {
   /// Returns the new instance of [TwitterException].
-  TwitterException(this.message, this.response);
+  TwitterException(this.message, this.response, [this.lastStreamedBody]);
 
   /// The error message
   final String message;
 
   /// The response from the Twitter API.
-  final Response response;
+  final BaseResponse response;
+
+  /// The body of the last received streamed request that caused this exception.
+  ///
+  /// `null` when the exception was caused by a non-streaming response.
+  final String? lastStreamedBody;
+
+  /// Returns the body of the [response] for non-streaming responses. Otherwise
+  /// returns the [lastStreamedBody].
+  String? get body =>
+      response is Response ? (response as Response).body : lastStreamedBody;
 
   @override
-  String toString() => '''TwitterException: $message
+  String toString() {
+    final buffer = StringBuffer()
+      ..writeln('TwitterException: $message\n')
+      ..writeln('  ✅ Status Code:')
+      ..writeln('   ${response.statusCode}\n')
+      ..writeln('  ✅ Request:')
+      ..writeln('   ${response.request}\n')
+      ..writeln('  ✅ Headers:')
+      ..writeln('   ${response.headers}\n');
 
-✅ Status Code:
-   ${response.statusCode}
+    if (body != null) {
+      buffer
+        ..writeln('  ✅ Body:')
+        ..writeln('   $body\n');
+    }
 
-✅ Request:
-   ${response.request}
+    buffer
+      ..writeln('  Please create an Issue if you have a question '
+          'or suggestion for this exception.')
+      ..writeln('  https://github.com/twitter-dart/twitter-api-v2/issues');
 
-✅ Headers:
-   ${response.headers}
-
-✅ Body:
-   ${response.body}
-
-Please create an Issue if you have a question or suggestion for this exception.
-https://github.com/twitter-dart/twitter-api-v2/issues
-''';
+    return buffer.toString();
+  }
 }
