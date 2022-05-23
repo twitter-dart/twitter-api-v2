033b4a52856b37afc5438b41dfc963b080b5ff9c
myConsciousness <kato.shinya.dev@gmail.com>
2022-05-23 11:20:02 +0900

    fix: ignore `invalid_annotation_target` (#42)

--- lib/src/service/lists/list_meta.dart
+++ lib/src/service/lists/list_meta.dart
@@ -2,6 +2,8 @@
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided the conditions.
 
+// ignore_for_file: invalid_annotation_target
+
 // Package imports:
 import 'package:freezed_annotation/freezed_annotation.dart';
 
