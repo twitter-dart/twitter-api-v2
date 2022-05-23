14ccaee63dc6c2ce3c21cf52699af334fb283f91
Kato Shinya / 加藤 真也 <kato.shinya.dev@gmail.com>
2022-05-23 09:03:23 +0900

    fix: renamed method to `connectVolumeStreams`

--- lib/src/service/tweets/tweets_service.dart
+++ lib/src/service/tweets/tweets_service.dart
@@ -978,7 +978,7 @@ abstract class TweetsService {
   /// ## Reference
   ///
   /// - https://developer.twitter.com/en/docs/twitter-api/tweets/volume-streams/api-reference/get-tweets-sample-stream
-  Future<Stream<TweetData>> volumeStreams({int? backfillMinutes, List<TweetExpansion>? expansions});
+  Future<Stream<TweetData>> connectVolumeStreams({int? backfillMinutes, List<TweetExpansion>? expansions});
 }
 
 class _TweetsService extends BaseService implements TweetsService {
@@ -1383,7 +1383,7 @@ class _TweetsService extends BaseService implements TweetsService {
       );
 
   @override
-  Future<Stream<TweetData>> volumeStreams({
+  Future<Stream<TweetData>> connectVolumeStreams({
     int? backfillMinutes,
     List<TweetExpansion>? expansions,
   }) async {
