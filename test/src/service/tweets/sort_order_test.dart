9c7b94aa3dd003a37a3f4c82570de9c23a84a3b7
myConsciousness <kato.shinya.dev@gmail.com>
2022-06-24 09:16:41 +0900

    test: added unit test

---
+++ test/src/service/tweets/sort_order_test.dart
@@ -0,0 +1,13 @@
+// Copyright 2022 Kato Shinya. All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided the conditions.
+
+import 'package:test/test.dart';
+import 'package:twitter_api_v2/src/service/tweets/sort_order.dart';
+
+void main() {
+  test('.name', () {
+    expect(SortOrder.recency.name, 'recency');
+    expect(SortOrder.relevancy.name, 'relevancy');
+  });
+}
