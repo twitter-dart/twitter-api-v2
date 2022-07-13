65ec0f1f926fcfda1c7bebcf96af89c0c7274a4a
myConsciousness <kato.shinya.dev@gmail.com>
2022-07-13 17:22:58 +0900

    test: added unit test

--- test/src/service/spaces/spaces_service_test.dart
+++ test/src/service/spaces/spaces_service_test.dart
@@ -122,24 +122,48 @@ void main() {
     expect(response.data.state, SpaceState.live);
   });
 
-  test('.lookupByIds', () async {
-    final spacesService = SpacesService(
-      context: context.buildGetStub(
-        UserContext.oauth2Only,
-        '/2/spaces',
-        'test/src/service/spaces/data/lookup_by_ids.json',
-        {'ids': '1DXxyRYNejbKM,2DXxyRYNejbKM'},
-      ),
-    );
+  group('.lookupByIds', () {
+    test('normal case', () async {
+      final spacesService = SpacesService(
+        context: context.buildGetStub(
+          UserContext.oauth2Only,
+          '/2/spaces',
+          'test/src/service/spaces/data/lookup_by_ids.json',
+          {'ids': '1DXxyRYNejbKM,2DXxyRYNejbKM'},
+        ),
+      );
 
-    final response = await spacesService.lookupByIds(
-      spaceIds: ['1DXxyRYNejbKM', '2DXxyRYNejbKM'],
-    );
+      final response = await spacesService.lookupByIds(
+        spaceIds: ['1DXxyRYNejbKM', '2DXxyRYNejbKM'],
+      );
 
-    expect(response, isA<TwitterResponse>());
-    expect(response.data, isA<List<SpaceData>>());
-    expect(response.data.first.id, '1DXxyRYNejbKM');
-    expect(response.data.first.state, SpaceState.live);
+      expect(response, isA<TwitterResponse>());
+      expect(response.data, isA<List<SpaceData>>());
+      expect(response.data.first.id, '1DXxyRYNejbKM');
+      expect(response.data.first.state, SpaceState.live);
+    });
+
+    test('with topic objects', () async {
+      final spacesService = SpacesService(
+        context: context.buildGetStub(
+          UserContext.oauth2Only,
+          '/2/spaces',
+          'test/src/service/spaces/data/lookup_by_ids_with_topics.json',
+          {'ids': '1DXxyRYNejbKM,2DXxyRYNejbKM'},
+        ),
+      );
+
+      final response = await spacesService.lookupByIds(
+        spaceIds: ['1DXxyRYNejbKM', '2DXxyRYNejbKM'],
+      );
+
+      expect(response, isA<TwitterResponse>());
+      expect(response.data, isA<List<SpaceData>>());
+      expect(response.includes?.topics, isNotNull);
+      expect(response.includes!.topics!.first.id, '847527650667094017');
+      expect(response.includes!.topics!.first.name, 'Gaming');
+      expect(response.includes!.topics!.first.description, 'All about gaming');
+    });
   });
 
   test('.lookupBuyers', () async {
