853bb355b7ec9334fd30fe740a4c883fee45501f
myConsciousness <kato.shinya.dev@gmail.com>
2022-07-14 17:49:47 +0900

    test: add unit tests

--- test/src/twitter_api_test.dart
+++ test/src/twitter_api_test.dart
@@ -12,40 +12,56 @@ import 'package:twitter_api_v2/src/service/spaces/spaces_service.dart';
 import 'package:twitter_api_v2/src/service/tweets/tweets_service.dart';
 import 'package:twitter_api_v2/src/service/users/users_service.dart';
 import 'package:twitter_api_v2/src/twitter_api.dart';
+import 'package:twitter_api_v2/twitter_api_v2.dart';
 
 void main() {
-  test('.tweetsService', () {
-    final twitter = TwitterApi(bearerToken: '');
+  group('services', () {
+    test('.tweetsService', () {
+      final twitter = TwitterApi(bearerToken: '');
 
-    expect(twitter.tweetsService, isNotNull);
-    expect(twitter.tweetsService, isA<TweetsService>());
+      expect(twitter.tweetsService, isNotNull);
+      expect(twitter.tweetsService, isA<TweetsService>());
+    });
+
+    test('.usersService', () {
+      final twitter = TwitterApi(bearerToken: '');
+
+      expect(twitter.usersService, isNotNull);
+      expect(twitter.usersService, isA<UsersService>());
+    });
+
+    test('.spacesService', () {
+      final twitter = TwitterApi(bearerToken: '');
+
+      expect(twitter.spacesService, isNotNull);
+      expect(twitter.spacesService, isA<SpacesService>());
+    });
+
+    test('.listsService', () {
+      final twitter = TwitterApi(bearerToken: '');
+
+      expect(twitter.listsService, isNotNull);
+      expect(twitter.listsService, isA<ListsService>());
+    });
+
+    test('.complianceService', () {
+      final twitter = TwitterApi(bearerToken: '');
+
+      expect(twitter.complianceService, isNotNull);
+      expect(twitter.complianceService, isA<ComplianceService>());
+    });
   });
 
-  test('.usersService', () {
-    final twitter = TwitterApi(bearerToken: '');
+  //! Since authentication information cannot be shared,
+  //! only that the communication is established is verified.
+  group('HTTP client in OAuth 2.0', () {
+    test('GET', () {
+      final twitter = TwitterApi(bearerToken: '');
 
-    expect(twitter.usersService, isNotNull);
-    expect(twitter.usersService, isA<UsersService>());
-  });
-
-  test('.spacesService', () {
-    final twitter = TwitterApi(bearerToken: '');
-
-    expect(twitter.spacesService, isNotNull);
-    expect(twitter.spacesService, isA<SpacesService>());
-  });
-
-  test('.listsService', () {
-    final twitter = TwitterApi(bearerToken: '');
-
-    expect(twitter.listsService, isNotNull);
-    expect(twitter.listsService, isA<ListsService>());
-  });
-
-  test('.complianceService', () {
-    final twitter = TwitterApi(bearerToken: '');
-
-    expect(twitter.complianceService, isNotNull);
-    expect(twitter.complianceService, isA<ComplianceService>());
+      expect(
+        () async => await twitter.tweetsService.lookupById(tweetId: ''),
+        throwsA(isA<TwitterException>()),
+      );
+    });
   });
 }
