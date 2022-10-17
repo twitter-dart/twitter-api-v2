d7c51a523b12b92d15f2ce9a20b4bae3f3fba79a
myConsciousness <kato.shinya.dev@gmail.com>
2022-10-17 21:54:18 +0900

    test: added unit tests

---
+++ test/src/pagination/forward_pagination_control_test.dart
@@ -0,0 +1,24 @@
+// Copyright 2022 Kato Shinya. All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided the conditions.
+
+// Package imports:
+import 'package:test/test.dart';
+
+// Project imports:
+import 'package:twitter_api_v2/src/pagination/forward_pagination_control.dart';
+import 'package:twitter_api_v2/src/pagination/pagination_control_type.dart';
+
+void main() {
+  test('.next', () {
+    final actual = ForwardPaginationControl.next();
+
+    expect(actual.type, PaginationControlType.forward);
+  });
+
+  test('.stop', () {
+    final actual = ForwardPaginationControl.stop();
+
+    expect(actual.type, PaginationControlType.stop);
+  });
+}
