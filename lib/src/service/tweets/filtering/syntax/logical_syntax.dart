12176fb213f37d20bf202267c156b37599221441
myConsciousness <kato.shinya.dev@gmail.com>
2022-10-20 12:00:59 +0900

    fix: added copyright headers

--- lib/src/service/tweets/filtering/syntax/logical_syntax.dart
+++ lib/src/service/tweets/filtering/syntax/logical_syntax.dart
@@ -1,3 +1,7 @@
+// Copyright 2022 Kato Shinya. All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided the conditions.
+
 // Project imports:
 import '../channel/post_logical_channel.dart';
 import '../filtering_rule_buffer.dart';
