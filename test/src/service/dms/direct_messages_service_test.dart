be9ea29ac3fcf05744638c9a9d4f34e99b60703b
myConsciousness <kato.shinya.dev@gmail.com>
2022-11-08 21:28:46 +0900

    test: added unit tests

--- test/src/service/dms/direct_messages_service_test.dart
+++ test/src/service/dms/direct_messages_service_test.dart
@@ -10,6 +10,8 @@ import 'package:twitter_api_core/twitter_api_core.dart';
 import 'package:twitter_api_v2/src/service/dms/direct_messages_service.dart';
 import 'package:twitter_api_v2/src/service/dms/dm_event_data.dart';
 import 'package:twitter_api_v2/src/service/dms/dm_event_meta.dart';
+import 'package:twitter_api_v2/src/service/dms/message_data.dart';
+import 'package:twitter_api_v2/src/service/dms/message_param.dart';
 import 'package:twitter_api_v2/src/service/pagination/pagination_control.dart';
 import 'package:twitter_api_v2/src/service/response/pagination_response.dart';
 import 'package:twitter_api_v2/src/service/response/twitter_response.dart';
@@ -566,6 +568,276 @@ void main() {
       expect(response.data.length, 4);
       expect(response.meta, isNotNull);
       expect(response.meta!.resultCount, 4);
+    });
+  });
+
+  group('.createMessageWith', () {
+    test('normal case', () async {
+      final directMessagesService = DirectMessagesService(
+        context: context.buildPostStub(
+          UserContext.oauth2OrOAuth1,
+          '/2/dm_conversations/with/1234/messages',
+          'test/src/service/dms/data/create_message_with.json',
+        ),
+      );
+
+      final response = await directMessagesService.createMessageWith(
+        participantId: '1234',
+        message: MessageParam(text: 'test'),
+      );
+
+      expect(response, isA<TwitterResponse>());
+      expect(response.data, isA<MessageData>());
+      expect(response.data.eventId, '1589951958990716934');
+      expect(response.data.conversationId,
+          '1514013247719952385-1514013247719952385');
+    });
+
+    test('with invalid access token', () async {
+      final directMessagesService = DirectMessagesService(
+        context: ClientContext(
+          bearerToken: '',
+          timeout: Duration(seconds: 10),
+        ),
+      );
+
+      expectUnauthorizedException(
+        () async => await directMessagesService.createMessageWith(
+          participantId: '1234',
+          message: MessageParam(text: 'test'),
+        ),
+      );
+    });
+
+    test('with rate limit exceeded error', () async {
+      final directMessagesService = DirectMessagesService(
+        context: context.buildPostStub(
+          UserContext.oauth2OrOAuth1,
+          '/2/dm_conversations/with/1234/messages',
+          'test/src/service/dms/data/rate_limit_exceeded_error.json',
+        ),
+      );
+
+      expectRateLimitExceededException(
+        () async => await directMessagesService.createMessageWith(
+          participantId: '1234',
+          message: MessageParam(text: 'test'),
+        ),
+      );
+    });
+
+    test('with errors', () async {
+      final directMessagesService = DirectMessagesService(
+        context: context.buildPostStub(
+          UserContext.oauth2OrOAuth1,
+          '/2/dm_conversations/with/1234/messages',
+          'test/src/service/dms/data/no_data.json',
+        ),
+      );
+
+      expectDataNotFoundExceptionDueToNoData(
+        () async => await directMessagesService.createMessageWith(
+          participantId: '1234',
+          message: MessageParam(text: 'test'),
+        ),
+      );
+    });
+
+    test('with no json', () async {
+      final directMessagesService = DirectMessagesService(
+        context: context.buildPostStub(
+          UserContext.oauth2OrOAuth1,
+          '/2/dm_conversations/with/1234/messages',
+          'test/src/service/dms/data/no_json.json',
+        ),
+      );
+
+      expectDataNotFoundExceptionDueToNoJson(
+        () async => await directMessagesService.createMessageWith(
+          participantId: '1234',
+          message: MessageParam(text: 'test'),
+        ),
+      );
+    });
+  });
+
+  group('.createMessageTo', () {
+    test('normal case', () async {
+      final directMessagesService = DirectMessagesService(
+        context: context.buildPostStub(
+          UserContext.oauth2OrOAuth1,
+          '/2/dm_conversations/1234/messages',
+          'test/src/service/dms/data/create_message_to.json',
+        ),
+      );
+
+      final response = await directMessagesService.createMessageTo(
+        conversationId: '1234',
+        message: MessageParam(text: 'test'),
+      );
+
+      expect(response, isA<TwitterResponse>());
+      expect(response.data, isA<MessageData>());
+      expect(response.data.eventId, '1589951958990716934');
+      expect(response.data.conversationId,
+          '1514013247719952385-1514013247719952385');
+    });
+
+    test('with invalid access token', () async {
+      final directMessagesService = DirectMessagesService(
+        context: ClientContext(
+          bearerToken: '',
+          timeout: Duration(seconds: 10),
+        ),
+      );
+
+      expectUnauthorizedException(
+        () async => await directMessagesService.createMessageTo(
+          conversationId: '1234',
+          message: MessageParam(text: 'test'),
+        ),
+      );
+    });
+
+    test('with rate limit exceeded error', () async {
+      final directMessagesService = DirectMessagesService(
+        context: context.buildPostStub(
+          UserContext.oauth2OrOAuth1,
+          '/2/dm_conversations/1234/messages',
+          'test/src/service/dms/data/rate_limit_exceeded_error.json',
+        ),
+      );
+
+      expectRateLimitExceededException(
+        () async => await directMessagesService.createMessageTo(
+          conversationId: '1234',
+          message: MessageParam(text: 'test'),
+        ),
+      );
+    });
+
+    test('with errors', () async {
+      final directMessagesService = DirectMessagesService(
+        context: context.buildPostStub(
+          UserContext.oauth2OrOAuth1,
+          '/2/dm_conversations/1234/messages',
+          'test/src/service/dms/data/no_data.json',
+        ),
+      );
+
+      expectDataNotFoundExceptionDueToNoData(
+        () async => await directMessagesService.createMessageTo(
+          conversationId: '1234',
+          message: MessageParam(text: 'test'),
+        ),
+      );
+    });
+
+    test('with no json', () async {
+      final directMessagesService = DirectMessagesService(
+        context: context.buildPostStub(
+          UserContext.oauth2OrOAuth1,
+          '/2/dm_conversations/1234/messages',
+          'test/src/service/dms/data/no_json.json',
+        ),
+      );
+
+      expectDataNotFoundExceptionDueToNoJson(
+        () async => await directMessagesService.createMessageTo(
+          conversationId: '1234',
+          message: MessageParam(text: 'test'),
+        ),
+      );
+    });
+  });
+
+  group('.createGroupConversation', () {
+    test('normal case', () async {
+      final directMessagesService = DirectMessagesService(
+        context: context.buildPostStub(
+          UserContext.oauth2OrOAuth1,
+          '/2/dm_conversations',
+          'test/src/service/dms/data/create_group_conversation.json',
+        ),
+      );
+
+      final response = await directMessagesService.createGroupConversation(
+        participantIds: ['aaa'],
+        message: MessageParam(text: 'test'),
+      );
+
+      expect(response, isA<TwitterResponse>());
+      expect(response.data, isA<MessageData>());
+      expect(response.data.eventId, '1589951958990716934');
+      expect(response.data.conversationId,
+          '1514013247719952385-1514013247719952385');
+    });
+
+    test('with invalid access token', () async {
+      final directMessagesService = DirectMessagesService(
+        context: ClientContext(
+          bearerToken: '',
+          timeout: Duration(seconds: 10),
+        ),
+      );
+
+      expectUnauthorizedException(
+        () async => await directMessagesService.createGroupConversation(
+          participantIds: ['aaa'],
+          message: MessageParam(text: 'test'),
+        ),
+      );
+    });
+
+    test('with rate limit exceeded error', () async {
+      final directMessagesService = DirectMessagesService(
+        context: context.buildPostStub(
+          UserContext.oauth2OrOAuth1,
+          '/2/dm_conversations',
+          'test/src/service/dms/data/rate_limit_exceeded_error.json',
+        ),
+      );
+
+      expectRateLimitExceededException(
+        () async => await directMessagesService.createGroupConversation(
+          participantIds: ['aaa'],
+          message: MessageParam(text: 'test'),
+        ),
+      );
+    });
+
+    test('with errors', () async {
+      final directMessagesService = DirectMessagesService(
+        context: context.buildPostStub(
+          UserContext.oauth2OrOAuth1,
+          '/2/dm_conversations',
+          'test/src/service/dms/data/no_data.json',
+        ),
+      );
+
+      expectDataNotFoundExceptionDueToNoData(
+        () async => await directMessagesService.createGroupConversation(
+          participantIds: ['aaa'],
+          message: MessageParam(text: 'test'),
+        ),
+      );
+    });
+
+    test('with no json', () async {
+      final directMessagesService = DirectMessagesService(
+        context: context.buildPostStub(
+          UserContext.oauth2OrOAuth1,
+          '/2/dm_conversations',
+          'test/src/service/dms/data/no_json.json',
+        ),
+      );
+
+      expectDataNotFoundExceptionDueToNoJson(
+        () async => await directMessagesService.createGroupConversation(
+          participantIds: ['aaa'],
+          message: MessageParam(text: 'test'),
+        ),
+      );
     });
   });
 }
