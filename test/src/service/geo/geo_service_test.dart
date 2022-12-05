4f9334270fd3a0bec3e811fa303acfce8bfa52de
myConsciousness <kato.shinya.dev@gmail.com>
2022-12-05 16:29:33 +0900

    test: add unit tests

--- test/src/service/geo/geo_service_test.dart
+++ test/src/service/geo/geo_service_test.dart
@@ -208,4 +208,120 @@ void main() {
       );
     });
   });
+
+  group('.lookupReverseGeocodedLocations', () {
+    test('normal case', () async {
+      final geoService = GeoService(
+        context: context.buildGetStub(
+          UserContext.oauth1Only,
+          '/1.1/geo/reverse_geocode.json',
+          'test/src/service/geo/data/lookup_reversed_geocoded_locations.json',
+          {
+            'lat': '10.0',
+            'long': '20.0',
+            'max_results': '10',
+            'accuracy': '5',
+            'granularity': 'country',
+          },
+        ),
+      );
+
+      final response = await geoService.lookupReverseGeocodedLocations(
+        latitude: 10.0,
+        longitude: 20.0,
+        maxResults: 10,
+        accuracy: '5',
+        granularity: GeoGranularity.country,
+      );
+
+      expect(response, isA<TwitterResponse>());
+      expect(response.data, isA<List<PlaceData>>());
+    });
+
+    test('with invalid access token', () async {
+      final geoService = GeoService(
+        context: ClientContext(
+          bearerToken: '',
+          oauthTokens: OAuthTokens(
+            consumerKey: '1234',
+            consumerSecret: '1234',
+            accessToken: '1234',
+            accessTokenSecret: '1234',
+          ),
+          timeout: Duration(seconds: 10),
+        ),
+      );
+
+      expectUnauthorizedException(
+        () async => await geoService.lookupReverseGeocodedLocations(
+          latitude: 10.0,
+          longitude: 20.0,
+        ),
+      );
+    });
+
+    test('with rate limit exceeded error', () async {
+      final geoService = GeoService(
+        context: context.buildGetStub(
+          UserContext.oauth1Only,
+          '/1.1/geo/reverse_geocode.json',
+          'test/src/service/geo/data/lookup_reversed_geocoded_locations.json',
+          statusCode: 429,
+          {
+            'lat': '10.0',
+            'long': '20.0',
+          },
+        ),
+      );
+
+      expectRateLimitExceededException(
+        () async => await geoService.lookupReverseGeocodedLocations(
+          latitude: 10.0,
+          longitude: 20.0,
+        ),
+      );
+    });
+
+    test('with errors', () async {
+      final geoService = GeoService(
+        context: context.buildGetStub(
+          UserContext.oauth1Only,
+          '/1.1/geo/reverse_geocode.json',
+          'test/src/service/geo/data/no_data.json',
+          {
+            'lat': '10.0',
+            'long': '20.0',
+          },
+        ),
+      );
+
+      expectDataNotFoundExceptionDueToNoData(
+        () async => await geoService.lookupReverseGeocodedLocations(
+          latitude: 10.0,
+          longitude: 20.0,
+        ),
+      );
+    });
+
+    test('with no json', () async {
+      final geoService = GeoService(
+        context: context.buildGetStub(
+          UserContext.oauth1Only,
+          '/1.1/geo/reverse_geocode.json',
+          'test/src/service/geo/data/no_json.json',
+          {
+            'lat': '10.0',
+            'long': '20.0',
+          },
+        ),
+      );
+
+      expectDataNotFoundExceptionDueToNoJson(
+        () async => await geoService.lookupReverseGeocodedLocations(
+          latitude: 10.0,
+          longitude: 20.0,
+        ),
+      );
+    });
+  });
 }
